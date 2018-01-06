991dfdc84e3135d4efd91f3a2ec7b4025565ee1b
Kelindon <35029811+Kelindon@users.noreply.github.com>
2018-01-06 12:42:36 +0100

    Update nodoFinal.m

--- nodoFinal.m
+++ nodoFinal.m
@@ -4,12 +4,12 @@ function [ vc_ref ] = nodoFinal( vmas, vc_inc, z0, zc)
 
 % Para seguir con la nomnclatura del nodo mitad
 % vmas seria v_left_inc
-% vc_inc sería v_right_inc
+% vc_inc serÃ­a v_right_inc
 
-gammama_source = (2.0*zc)/(z0+zc);
+gamma_source = (2.0*zc)/(z0+zc);
 gamma_inc = (z0-zc)/(z0+zc);
 
-vc_ref = gammama_source*vmas+gamma_inc*vc_inc;
+vc_ref = gamma_source*vmas+gamma_inc*vc_inc;
 
 
 
